d8d63a892a060289a6bb61f2a6fac056d54c4a08
erwanjb <wawan.jb@gmail.com>
2020-12-15 15:27:26 +0100

    mailgun host modif

--- server/configs/nodemailer.config.ts
+++ server/configs/nodemailer.config.ts
@@ -4,9 +4,9 @@ import mailer from "nodemailer";
 const auth = {
     auth: {
       api_key: process.env.MAILGUN_API_KEY,
-      domain: process.env.MAILGUN_DOMAINE,
-      host: process.env.MAILGUN_HOST,
-    }
+      domain: process.env.MAILGUN_DOMAINE
+    },
+    host: process.env.MAILGUN_HOST
 }
 
 const transport = mailer.createTransport(mg(auth));
